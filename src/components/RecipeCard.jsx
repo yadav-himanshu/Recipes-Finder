e1978d034dc635cf7b786348ca26ca28ea195ceb
Himanshu Yadav <himanshuyadav7852@gmail.com>
2024-10-19 16:18:51 +0530

    Search and Favorites Added

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,33 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Search,Soup, Heart,HeartPulse } from 'lucide-react'
-const RecipeCard = () => {
+
+const getTwoValueFromArray=(arr)=>{
+  return [arr[0],arr[1]]
+}
+const RecipeCard = ({recipe,bg,badge}) => {
+  const healthLabels=getTwoValueFromArray(recipe.healthLabels);
+
+  const [isFavorite, setFavorite]=useState(localStorage.getItem('favorites')?.includes(recipe.label))
+
+  const addRecipeToFavorite=()=>{
+    let favorites=JSON.parse(localStorage.getItem('favorites'))||[];
+    const isRecipeAlreadyInFavorites=favorites.some((fav)=>fav.label===recipe.label);
+
+    if(isRecipeAlreadyInFavorites){
+      favorites=favorites.filter((fav)=>fav.label !==recipe.label)
+      setFavorite(false)
+    }
+    else{
+      favorites.push(recipe)
+      setFavorite(true)
+    }
+
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+  }
   return (
-    <div className='flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative'>
-          <a href="#" className='relative h-32'>
-            <img src="/1.jpg" alt="Recipe Image" className='w-full rounded-md h-full object-cover cursor-pointer'/>
+    <div className={`flex flex-col rounded-md ${bg} overflow-hidden p-3 relative`}>
+          <a 
+          href={`https://www.youtube.com/results?search_query=${recipe.label} recipe`} 
+          target='_blank'
+          className='relative h-32'>
+            <div className='skeleton absolute inset-0'/>
+            <img src={recipe.image} 
+            alt="Recipe Image" 
+            className='w-full rounded-md h-full object-cover cursor-pointer opacity-0 transition-opacity duration-500'
+            onLoad={(e)=>{
+              e.currentTarget.style.opacity=1
+              e.currentTarget.previousElementSibling.style.display="none";
+            }}
+            />
             <div className='absolute rounded-full flex gap-1 bottom-2 left-2 bg-white items-center p-1 text-sm'>
-              <Soup size={'16'}/> 4 Servings
+              <Soup size={'16'}/> {recipe.yield} Servings
             </div>
-            <div className='absolute top-1 right-2 rounded-full p-1 bg-white cursor-pointer'>
-              <Heart size={'20'} className={'hover:fill-red-600 hover:text-red-600'}/>
+            <div className='absolute top-1 right-2 rounded-full p-1 bg-white cursor-pointer'
+              onClick={(e)=>{
+                e.preventDefault();
+                addRecipeToFavorite()
+            }}>
+              {!isFavorite && <Heart size={'20'} className={'hover:fill-red-600 hover:text-red-600'}/>}
+              {isFavorite && <Heart size={'20'} className={'fill-red-600 text-red-600'}/>}
+              
 
             </div>
 
           </a>
 
           <div className='flex mt-1'>
-            <p className='font-bold tracking-wide'>Roasted Chicken</p>
+            <p className='font-bold tracking-wide'>{recipe.label}</p>
           </div>
-          <p className='my-2' >Turkish Kitchen</p>
+          <p className='my-2' >{
+                recipe.cuisineType[0].charAt(0).toUpperCase()+recipe.cuisineType[0].slice(1)
+                } Kitchen</p>
           <div className='flex gap-2 mt-auto'>
-            <div className='flex gap-1 bg-[#d6f497] items-center rounded-md p-1'>
+
+            {
+              healthLabels.map((label,idx)=>(
+                <div key={idx} className={`flex gap-1 ${badge} items-center rounded-md p-1`}>
+                  <HeartPulse size={'16'}/>
+                  <span className='tracking-tighter text-sm font-semibold'>{label}</span>
+                </div>
+              ))
+            }
+
+            {/* <div className='flex gap-1 bg-[#d6f497] items-center rounded-md p-1'>
               <HeartPulse size={'16'}/>
               <span className='tracking-tighter text-sm font-semibold'>Gluten-Free</span>
             </div>
             <div className='flex gap-1 bg-[#d6f497] items-center rounded-md p-1'>
               <HeartPulse size={'16'}/>
               <span className='tracking-tighter text-sm font-semibold'>Heart Healthy</span>
-            </div>
+            </div> */}
           </div>
 
         </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,46 @@
 import { Search,Soup, Heart,HeartPulse } from 'lucide-react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import RecipeCard from '../components/RecipeCard'
+import { getRandomColor } from '../lib/utils'
+
+const APP_ID = import.meta.env.VITE_APP_ID;
+const APP_KEY = import.meta.env.VITE_APP_KEY;
 
 const HomePage = () => {
+  const[recipes,setRecipes]=useState([]);
+  const [loading, setLoading]=useState(true)
+
+  const fetchRecipes=async(searchQuery)=>{
+    setLoading(true);
+    setRecipes([]);
+    try{
+      const res = await fetch(`https://api.edamam.com/api/recipes/v2/?app_id=${APP_ID}&app_key=${APP_KEY}&q=${searchQuery}&type=public`);
+      const data=await res.json();
+      console.log(data)
+      setRecipes(data.hits)
+    }
+    catch(error){
+      console.log(error.message)
+    }
+    finally{
+      setLoading(false)
+    }
+  }
+
+  useEffect(()=>{
+    fetchRecipes('chicken')
+  },[])
+
+
+  const handleSearchRecipe=(e)=>{
+    e.preventDefault();
+    fetchRecipes(e.target[0].value);
+  }
+
   return (
     <div className='bg-[#faf9fb] p-10 flex-1' >
       <div className='max-w-screen-lg mx-auto'>
-        <form action="">
+        <form onSubmit={handleSearchRecipe} action="">
           <label htmlFor="" className='input shadow-md flex items-center gap-2'>
             <Search size={'24'}/>
             <input type="text" className='text-sm md:text-md grow' placeholder='What do you want to cook today' />
@@ -16,12 +50,23 @@ const HomePage = () => {
         <p className='text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight'>Popular Choices</p>
 
         <div className='grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-          <RecipeCard/>   
-          <RecipeCard/> 
-          <RecipeCard/> 
-          <RecipeCard/> 
-          <RecipeCard/> 
-          <RecipeCard/> 
+          {!loading && 
+            recipes.map(({recipe},index)=>(
+              <RecipeCard key={index} recipe={recipe} {...getRandomColor()}/>
+              
+            ))  
+          }
+
+          {loading &&
+            [...Array(9)].map((_,index)=>(
+              <div key={index} className="flex w-52 flex-col gap-4">
+                <div className="skeleton h-32 w-full"></div>
+                <div className="skeleton h-4 w-28"></div>
+                <div className="skeleton h-4 w-full"></div>
+                <div className="skeleton h-4 w-full"></div>
+              </div>
+            ))
+          }
              
         </div>
       </div>
